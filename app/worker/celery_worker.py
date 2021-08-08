df21c91e2c41f409b580c47a094dcdd0574f39f0
José C. Paiva <josepaiva94@gmail.com>
2021-08-08 16:48:28 +0100

    ffmpeg deps and sample

--- app/worker/celery_worker.py
+++ app/worker/celery_worker.py
@@ -1,4 +1,6 @@
+import sys
 from time import sleep
+import ffmpeg
 
 from celery import current_task
 
@@ -11,4 +13,21 @@ def test_celery(word: str) -> str:
         sleep(1)
         current_task.update_state(state='PROGRESS',
                                   meta={'process_percent': i*10})
+    generate_thumbnail('/app/in.mp4', 'out.jpg', 0.1, 120)
     return f"test task return {word}"
+
+
+def generate_thumbnail(in_filename, out_filename, time, width):
+    try:
+        (
+            ffmpeg
+            .input(in_filename, ss=time)
+            .filter('scale', width, -1)
+            .output(out_filename, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+    except ffmpeg.Error as e:
+        print(e.stderr.decode(), file=sys.stderr)
+        sys.exit(1)
+
